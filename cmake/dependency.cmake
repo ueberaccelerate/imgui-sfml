6ea68cba7490b8621b39cb062d461521212b24c3
ueberaccelerate <ueberaccelerate@gmail.com>
2020-05-26 13:57:32 +0300

    -- cmake updated

---
+++ cmake/dependency.cmake
@@ -0,0 +1,13 @@
+include ( cmake/CPM.cmake )
+
+CPMAddPackage(
+  NAME imgui
+  GITHUB_REPOSITORY ueberaccelerate/imgui
+  GIT_TAG master
+)
+
+function (set_dependency target_name target_type ) 
+
+target_link_libraries( ${target_name} ${target_type} imgui )
+
+endfunction()
